69a8158994f9a73394526fd2bcd94951ce010f1d
AbhishekMishra <abmishra700@gmail.com>
2023-11-28 19:04:26 +0530

    nest test

---
+++ src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { UsersModule } from './users/users.module';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { User } from './users/users.model';
+
+@Module({
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'test',
+      models: [User],
+      autoLoadModels: true,
+      synchronize: true,
+      sync: { force: true },
+    }),
+    UsersModule,
+  ],
+})
+export class AppModule {}
